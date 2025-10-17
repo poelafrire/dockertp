5ae303d4a12d9e138b9022aa90c304a9cb9bd8a9
poelafrire <gginttt@gmail.com>
2025-10-17 10:43:50 +0200

    q

---
+++ results.bash
@@ -0,0 +1,29 @@
+$ sudo docker pull alpine
+Using default tag: latest
+latest: Pulling from library/alpine
+2d35ebdb57d9: Pull complete 
+Digest: sha256:4b7ce07002c69e8f3d704a9c5d6fd3053be500b7f1c69fc0d80990c2ad8dd412
+Status: Downloaded newer image for alpine:latest
+docker.io/library/alpine:latest
+
+
+total 4
+drwxr-xr-x    1 root     root           858 Oct  8 09:28 bin
+drwxr-xr-x    5 root     root           340 Oct 17 08:30 dev
+drwxr-xr-x    1 root     root            56 Oct 17 08:30 etc
+drwxr-xr-x    1 root     root             0 Oct  8 09:28 home
+drwxr-xr-x    1 root     root           146 Oct  8 09:28 lib
+drwxr-xr-x    1 root     root            28 Oct  8 09:28 media
+drwxr-xr-x    1 root     root             0 Oct  8 09:28 mnt
+drwxr-xr-x    1 root     root             0 Oct  8 09:28 opt
+dr-xr-xr-x  569 root     root             0 Oct 17 08:30 proc
+drwx------    1 root     root             0 Oct  8 09:28 root
+drwxr-xr-x    1 root     root             8 Oct  8 09:28 run
+drwxr-xr-x    1 root     root           790 Oct  8 09:28 sbin
+drwxr-xr-x    1 root     root             0 Oct  8 09:28 srv
+dr-xr-xr-x   13 root     root             0 Oct 17 06:02 sys
+drwxrwxrwt    1 root     root             0 Oct  8 09:28 tmp
+drwxr-xr-x    1 root     root            40 Oct  8 09:28 usr
+drwxr-xr-x    1 root     root            86 Oct  8 09:28 var
+
+
